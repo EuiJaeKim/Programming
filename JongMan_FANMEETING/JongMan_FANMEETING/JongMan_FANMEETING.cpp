86fc88eb446072692ef26081449a64d268918cd1
xcome2ya <xcome2ya@naver.com>
2018-05-16 21:42:28 +0900

    Devide&Conquer Solve는 반으로 나누어서 풀기 J~는 종만북에 있는거 SolveT는 M 위치에서만 검사. 3개 다 시간초과. 흠..

--- JongMan_FANMEETING/JongMan_FANMEETING/JongMan_FANMEETING.cpp
+++ JongMan_FANMEETING/JongMan_FANMEETING/JongMan_FANMEETING.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
 using namespace std;
 
 /*
@@ -12,14 +13,145 @@ M은 해당하는 사람이 남자, F는 해당하는 사람이 여자임을 나타냅니다. 멤버의 수와 �
 각 테스트 케이스마다 한 줄에 모든 멤버들이 포옹을 하는 일이 몇 번이나 있는지 출력합니다.
 */
 
+/*
+
+반으로 나눌때 if의 조건은 최소길이 이고 left right로 나눌때 left의 끝점은 Start에 (End - Start) / 2을 더해야한다.
+right의 시작점 또한 Start에 (End - Start) / 2을 기준으로 시작해야한다.
+잊지말장
+
+if ((End - Start) / 2 >= HyperSenior.size()) {
+LeftSum = Solve(Start, Start + (End - Start) / 2);
+RightSum = Solve(Start+((End - Start) / 2), End);
+}
+*/
+
+string HyperSenior;
+string Fans;
+
+int Solve(int Start, int End);
+int JongManSolve();
+int SolveT();
+bool Check(int Start);
+
 int main()
 {
 	int TC;
+	char Input;
 
 	cin >> TC;
 	for (int i = 0; i < TC; i++) {
+		HyperSenior.resize(200000);
+		Fans.resize(200000);
+		cin >> HyperSenior;
+		cin >> Fans;
 
+		/*cout << Solve(0, Fans.size()) << endl;
+		cout << JongManSolve() << endl;*/
+		cout << SolveT() << endl;
+		HyperSenior.clear();
+		Fans.clear();
 	}
 
 	return 0;
+}
+
+int JongManSolve()
+{
+	int N = HyperSenior.size() + Fans.size() - 1;
+	int Compare = 0;
+	vector<int> A(HyperSenior.size()), B(N, 0);
+	for (int i = 0; i < HyperSenior.size(); i++) {
+		if (HyperSenior[i] == 'M') {
+			A[i] = 1;
+		}
+
+	}
+
+	int Cur = 0;
+	for (int i = 0; i < Fans.size(); i++) {
+		if (Fans[i] == 'M')
+		{
+			for (int j = A.size() - 1; j >= 0; j--) {
+				B[Cur + (A.size() - 1 - j)] += A[j];
+			}
+		}
+		Cur++;
+	}
+
+	int Ret = 0;
+	for (int i = HyperSenior.size() - 1; i < Fans.size(); i++)
+		if (B[i] == 0)
+			Ret++;
+	A.clear();
+	B.clear();
+	return Ret;
+}
+
+int SolveT()
+{
+	/*
+	1. 배열 A는 HyperSenior의 M의 갯수만큼의 길이를 가지고있고, 각 원소의 값은 HyperSenior배열에서 M의 위치.
+	2. 배열 Fans를 기준으로 M이 나올때마다 서치. M의 갯수만큼만.
+	*/
+	vector<int> A;
+	bool CountUp;
+	int Count=0;
+
+	for (int i = 0; i < HyperSenior.size(); i++) {
+		if (HyperSenior[i] == 'M')
+			A.push_back(i);
+	}
+	if (A.size() != 0) {
+		for (int i = A[0]; i + HyperSenior.size() - 1 - A[0] < Fans.size(); i++) {
+			if (Fans[i] == 'F') {
+				CountUp = true;
+				for (int j = 1; j < A.size(); j++) {
+					if (Fans[i + (A[j] - A[0])] == 'M') {
+						CountUp = false;
+						break;
+					}
+				}
+				if (CountUp)
+					Count++;
+			}
+		}
+	}
+	else {
+		Count = Fans.size() - HyperSenior.size() + 1;
+	}
+	A.clear();
+
+	return Count;
+}
+
+int Solve(int Start, int End)
+{
+	int LeftSum, RightSum;
+	LeftSum = RightSum = 0;
+
+	if ((End - Start) / 2 >= HyperSenior.size()) {
+		LeftSum = Solve(Start, Start + (End - Start) / 2);
+		RightSum = Solve(Start + ((End - Start) / 2), End);
+	}
+
+	int Count = 0;
+	for (int StartCur = Start + ((End - Start) / 2) - 1; StartCur >= Start; StartCur--) {
+		if (StartCur + HyperSenior.size() <= End && StartCur + HyperSenior.size() > Start + (End - Start) / 2) {
+			if (Check(StartCur)) {
+				Count++;
+			}
+		}
+	}
+	return Count + LeftSum + RightSum;
+}
+
+bool Check(int Start)
+{
+	bool Res = true;
+	for (int i = 0; i < HyperSenior.size(); i++) {
+		if (HyperSenior[i] == 'M' && Fans[Start + i] == 'M') {
+			return Res = false;
+		}
+	}
+	return Res;
 }
