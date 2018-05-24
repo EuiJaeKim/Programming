36d38462c80f07209e561d00861dac50f1df0a5a
xcome2ya <xcome2ya@naver.com>
2018-05-24 15:13:25 +0900

    SWEA_D3_1491 다음에 풀꺼임

---
+++ SWEA_D3_1491/SWEA_D3_1491/SWEA_D3_1491.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int N;
+int A;
+int B;
+
+int main()
+{
+	int TC;
+
+	cin >> TC;
+	for (int i = 1; i <= TC; i++) {
+
+	}
+
+	return 0;
+}
